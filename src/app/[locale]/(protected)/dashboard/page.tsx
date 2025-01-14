9890dba70b0abd9b097b86be94952a4295785420
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-14 22:34:28 +0400

    designing the dashboard

--- src/app/[locale]/(protected)/dashboard/page.tsx
+++ src/app/[locale]/(protected)/dashboard/page.tsx
@@ -1,34 +1,41 @@
 import NavBar from "./NavBar";
 import { auth } from "../../../../../auth";
 import Image from "next/image";
-import img1 from "../../../../../public/img1.jpeg"
+import img2 from "../../../../../public/img2.jpeg"
 export default async function Dashboard(){
       const session = await auth();
       if (!session) return null;
     
     return(
         <div >
-            <div className="">
-            <div>
+            <div className="grid grid-cols-3 h-screen">
+            <div className="bg-violet-200">
                 <NavBar userId={session.user?.id}/>
             </div>
            
-            <div>
+            <div className="bg-orange-200 col-span-2 flex flex-col items-center p-4">
+                <div className="flex flex-col justify-center items-center w-[40vw] h-fit p-8 mt-32 rounded-3xl bg-slate-400">
                 <div>
-                <p>{session.user?.email}</p>
-                <p>{session.user?.id}</p>
+               
+              
                 {
                     session.user?.image == null ?(
                         <div>
-                        <Image src={img1} alt="use profile img" height={30} width={30}/>
+                        <Image src={img2} alt="use profile img" height={40} width={40}/>
                         </div>
                     ):(
                
                         <div>                  
-                             <Image src={session.user?.image} alt="use profile img" height={30} width={30}/>
+                             <Image src={session.user?.image} alt="use profile img" height={40} width={40}/>
                         </div> 
                     )}               
                 </div>
+                <div>
+                    <p className="text-sm md:text-xl ">{session.user?.email}</p>
+                </div>
+
+                </div>
+                
             </div>
           </div>
         </div>
