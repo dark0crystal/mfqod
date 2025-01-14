142f6c401a3749014a283f7c84093a479a91207d
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-14 20:50:52 +0400

    adding user info

--- src/app/[locale]/(protected)/dashboard/page.tsx
+++ src/app/[locale]/(protected)/dashboard/page.tsx
@@ -1,11 +1,36 @@
+import NavBar from "./NavBar";
+import { auth } from "../../../../../auth";
 import Image from "next/image";
-
-export default function Dashboard(){
+import img1 from "../../../../../public/img1.jpeg"
+export default async function Dashboard(){
+      const session = await auth();
+      if (!session) return null;
     
-
     return(
-        <div>
-admin
+        <div >
+            <div className="">
+            <div>
+                <NavBar userId={session.user?.id}/>
+            </div>
+           
+            <div>
+                <div>
+                <p>{session.user?.email}</p>
+                <p>{session.user?.id}</p>
+                {
+                    session.user?.image == null ?(
+                        <div>
+                        <Image src={img1} alt="use profile img" height={30} width={30}/>
+                        </div>
+                    ):(
+               
+                        <div>                  
+                             <Image src={session.user?.image} alt="use profile img" height={30} width={30}/>
+                        </div> 
+                    )}               
+                </div>
+            </div>
+          </div>
         </div>
     )
 }
