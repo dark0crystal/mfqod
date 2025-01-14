df614f90a17fa02c6bf9c355760fe2a7479cf459
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-14 22:43:30 +0400

    addding portfolio img

--- src/app/[locale]/(protected)/dashboard/page.tsx
+++ src/app/[locale]/(protected)/dashboard/page.tsx
@@ -9,32 +9,36 @@ export default async function Dashboard(){
     return(
         <div >
             <div className="grid grid-cols-3 h-screen">
-            <div className="bg-violet-200">
-                <NavBar userId={session.user?.id}/>
-            </div>
-           
-            <div className="bg-orange-200 col-span-2 flex flex-col items-center p-4">
-                <div className="flex flex-col justify-center items-center w-[40vw] h-fit p-8 mt-32 rounded-3xl bg-slate-400">
+            <div className="bg-violet-200 flex flex-col  items-center px-4">
+            <div className="flex flex-col justify-center items-center w-full h-fit p-8 mt-12 rounded-3xl bg-slate-400">
                 <div>
                
               
                 {
                     session.user?.image == null ?(
-                        <div>
-                        <Image src={img2} alt="use profile img" height={40} width={40}/>
+                        <div className="relative rounded-full w-[60px] h-[60px] overflow-hidden">
+                            <Image src={img2} alt="use profile img" className="absolute" fill objectFit="cover"/>
                         </div>
                     ):(
                
-                        <div>                  
-                             <Image src={session.user?.image} alt="use profile img" height={40} width={40}/>
+                        <div className="relative rounded-full w-[60px] h-[60px] overflow-hidden">                  
+                             <Image src={session.user?.image} alt="use profile img" className="absolute" fill objectFit="cover"/>
                         </div> 
                     )}               
                 </div>
                 <div>
+                    <p className="text-sm md:text-xl ">{session.user?.name}</p>
+                </div>
+                <div>
                     <p className="text-sm md:text-xl ">{session.user?.email}</p>
                 </div>
-
+                
                 </div>
+                <NavBar userId={session.user?.id}/>
+            </div>
+           
+            <div className="bg-orange-200 col-span-2 flex flex-col items-center p-4">
+                
                 
             </div>
           </div>
