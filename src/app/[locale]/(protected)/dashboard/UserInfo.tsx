f02db5f10c29e6abea9f95e665d10ec9e6867e6f
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-14 23:22:04 +0400

    adding component to show user info

---
+++ src/app/[locale]/(protected)/dashboard/UserInfo.tsx
@@ -0,0 +1,47 @@
+import { auth } from "../../../../../auth"
+import Image from "next/image"
+import img2 from "../../../../../public/img2.jpeg"
+export default async function UserInfo(){
+
+    const session = await auth()
+    if(!session) return null
+
+    return(
+       <>
+        <div className="bg-violet-200 flex flex-col  items-center px-4">
+        <div className="flex flex-col justify-center items-center w-full h-fit p-8 mt-12 rounded-3xl bg-slate-400/40">
+            <div>
+           
+          
+            {
+                session.user?.image == null ?(
+                    <div className="relative rounded-full w-[60px] h-[60px] overflow-hidden">
+                        <Image src={img2} alt="use profile img" className="absolute" fill objectFit="cover"/>
+                    </div>
+                ):(
+           
+                    <div className="relative rounded-full w-[60px] h-[60px] overflow-hidden">                  
+                         <Image src={session.user?.image} alt="use profile img" className="absolute" fill objectFit="cover"/>
+                    </div> 
+                )}               
+            </div>
+            <div>
+                <p className="text-sm md:text-xl ">{session.user?.name}</p>
+            </div>
+            <div>
+                <p className="text-sm text-black/70 md:text-lg ">{session.user?.email}</p>
+            </div>
+            
+            </div>
+          
+        </div>
+       
+        <div className="bg-orange-200 col-span-2 flex flex-col items-center p-4">
+            
+            
+        </div>
+        </>
+        
+
+    )
+}
