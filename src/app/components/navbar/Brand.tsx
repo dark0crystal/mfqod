8ce7dfb8000f94e288ed4c82007a6dc87d92cbd6
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-14 16:19:33 +0400

    navbar

---
+++ src/app/components/navbar/Brand.tsx
@@ -0,0 +1,24 @@
+
+// import { getTranslations } from "next-intl/server";
+// import { Link } from "@/i18n/routing";
+import { Lalezar } from "next/font/google";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+// import { getLocale } from "next-intl/server";
+
+const lalezarFont = Lalezar({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap",
+  });
+
+export default  function Brand(){
+    const t =useTranslations("navbar")
+    // const locale = (await getLocale()).substring(0,2)
+    // const t = await getTranslations("HomePage")
+    return(
+        <div className="mx-6 text-6xl ">
+            <Link   className={lalezarFont.className} href='/'>{t("brand")}</Link>
+        </div>
+    )
+}
---
+++ src/app/components/navbar/NavMenu.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+// import { useSession } from "next-auth/react";
+// import SignIn from "../SignIn";
+// import SignOut from "../SignOut";
+
+
+   
+export default function NavMenu(){
+    // const session =  useSession();
+    return(
+        <div></div>
+        // <div className="bg-black text-[#fbda5f] p-3 rounded-full"> 
+        // {session?.status ==='authenticated' ? (<SignOut/>):(<SignIn/>)}
+        
+
+        // </div>   
+       
+    )
+}
