50fce122aa7f015f1edfb6f6d77215283d342a62
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-14 23:21:39 +0400

    adding the dashboard layout

--- src/app/[locale]/(protected)/dashboard/layout.tsx
+++ src/app/[locale]/(protected)/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import NavBar from "./NavBar";
 import { auth } from "../../../../../auth";
+import img2 from "../../../../../public/img2.jpeg"
+import UserInfo from "./UserInfo";
 export default async function Layout({children}: Readonly<{ children: React.ReactNode;}>)  {
     const session = await auth();
     if (!session) return null;
@@ -7,8 +9,12 @@ export default async function Layout({children}: Readonly<{ children: React.Reac
     <>
 
       <main>
+      <div className="grid grid-cols-3 h-screen">
+        <UserInfo/>
         <NavBar userId={session.user?.id}/>
+
         {children}
+        </div>
       </main>
 
     </>
