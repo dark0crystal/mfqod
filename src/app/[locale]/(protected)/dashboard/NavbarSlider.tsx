c72d577cd5a375473b18aa9c4d7a3cb449e98ac5
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-16 17:05:37 +0400

    adding params

--- src/app/[locale]/(protected)/dashboard/NavbarSlider.tsx
+++ src/app/[locale]/(protected)/dashboard/NavbarSlider.tsx
@@ -2,7 +2,7 @@
 import { Link } from "@/i18n/routing";
 import { orgName } from "../../search/page"; // Assuming orgName contains a list of organization names
 
-export default function NavbarSlider({ userRole, managedOrgs }: { userRole: string; managedOrgs?: string[] }) {
+export default function NavbarSlider({ userRole, managedOrg , managedPlace }: { userRole: string; managedOrg?: any ,managedPlace?: any }) {
   const renderLinks = () => {
     switch (userRole) {
       case "TECHADMIN":
@@ -37,34 +37,40 @@ export default function NavbarSlider({ userRole, managedOrgs }: { userRole: stri
         ));
       case "VERIFIED":
         // Access only to managed organizations
-        return managedOrgs?.map((org, index) => (
+        return(
+            <div>
           <Link
-            key={index}
+         
             href={{
               pathname: "/dashboard/posts",
-              query: { orgName: org }, // Pass orgName as a query
+              query: { orgName: managedOrg}, // Pass orgName as a query
             }}
           >
             <div className="p-4 bg-yellow-500 text-white rounded-lg my-2 cursor-pointer">
-              {org}
+              {managedOrg}
             </div>
           </Link>
-        ));
+           <Link
+         
+           href={{
+             pathname: "/dashboard/posts",
+             query: { orgName: managedOrg ,placeName:managedPlace}, // Pass orgName as a query
+           }}
+         >
+           <div className="p-4 bg-yellow-500 text-white rounded-lg my-2 cursor-pointer">
+             {managedPlace}
+           </div>
+         </Link>
+         </div>
+        )
+
       case "BASIC":
         // Access only to their added places
-        return managedOrgs?.map((org, index) => (
-          <Link
-            key={index}
-            href={{
-              pathname: "/dashboard/posts",
-              query: { orgName: org }, // Pass orgName as a query
-            }}
-          >
-            <div className="p-4 bg-purple-500 text-white rounded-lg my-2 cursor-pointer">
-              {org}
+        return (
+            <div>
+                <h1>You are a normal user</h1>
             </div>
-          </Link>
-        ));
+        )
       default:
         return <p>No access available</p>;
     }
