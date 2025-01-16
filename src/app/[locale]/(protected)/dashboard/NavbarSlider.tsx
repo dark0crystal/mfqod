96bb27de83ebfef52bc69b0f2740fa0ada748c54
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-16 10:37:40 +0400

    specific navbar for different user

--- src/app/[locale]/(protected)/dashboard/NavbarSlider.tsx
+++ src/app/[locale]/(protected)/dashboard/NavbarSlider.tsx
@@ -1,14 +1,30 @@
-"use Client"
+
 import { orgName } from "../../search/page";
 
 export default function NavbarSlider(params:any){
 
     const {userRole} = params;
-
-
+//Using the user role , give for each user a different navbar
+//
     return(
         <div>
 
+            {/* Tech Admin , give him the full access to all places and users  */}
+            <div>
+
+            </div>
+            {/*  Admin , give him the full access to all places only */}
+            <div>
+                
+            </div>
+            {/* Manager , give him the  access to the place he/she manage */}
+            <div>
+                
+            </div>
+            {/* Basic , give him the  access to all places he/she add  */}
+            <div>
+                
+            </div>
         </div>
     )
 }
