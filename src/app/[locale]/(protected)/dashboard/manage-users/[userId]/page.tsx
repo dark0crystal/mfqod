e3fd714de27b3b759abceaeb49ec12551f7f98e5
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-17 14:07:43 +0400

    adding edit profile user page

---
+++ src/app/[locale]/(protected)/dashboard/manage-users/[userId]/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { useForm } from "react-hook-form"
+
+
+type FormFields ={
+    role:string
+    managePlace:string
+    manageOrg:string
+    
+}
+export default function EditUserProfile(){
+
+
+
+    return(
+        <div>
+
+        </div>
+    )
+}
