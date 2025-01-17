29f88c6c9cd54ac5a3d338c264c6a5665e340a4e
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-17 11:25:59 +0400

    adding zod validation

--- src/app/[locale]/(protected)/dashboard/manageUsers/page.tsx
+++ src/app/[locale]/(protected)/dashboard/manageUsers/page.tsx
@@ -1,17 +1,23 @@
 "use client"
+import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form"
+import {z} from "zod"
 
 
-type FormFields = {
-    email:string
-}
+const schema = z.object({
+    email :z.string()
+})
+type FormFields =z.infer<typeof schema>;
+
 export default function ManageUsers(){
-    const {register , handleSubmit} = useForm<FormFields>();
+    const {register , handleSubmit , setError ,formState:{errors ,isSubmitting}} = useForm<FormFields>({
+        resolver:zodResolver(schema),
+    });
 
     const onSubmit:SubmitHandler<FormFields>= (data)=>{
         console.log(data)
     }
-    
+
     return(
         <div>
             
@@ -19,6 +25,9 @@ export default function ManageUsers(){
             <div>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <input type="text" {...register("email")}/>
+                    {errors.email &&(
+                        <div className="text-red-400">{errors.email.message}</div>
+                    )}
                     <button type="submit">Search</button>
                 </form>
             </div>
