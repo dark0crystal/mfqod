2374208b55ba9fa4f35881a2b189f20bf9110f51
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-17 15:06:02 +0400

    adding orgPlaces

---
+++ src/app/storage.tsx
@@ -0,0 +1,11 @@
+// Here we store the data that is used by multiple components
+
+export const orgName = ["SQU","UTAS Muscat", "UTAS Ibra","Bin Omair Library" , "UTAS Nizwa"]
+
+
+// used in report-found-item , 
+  export const OrgPlaces = [
+    { "SQU": ["SQU Library", "SQU Lost and Found Department"] },
+    { "UTAS Muscat": ["UTAS Library", "UTAS Lost and Found Department"] },
+    { "Bin Omair": ["Bin Omair Library", "Bin Omair Lost and Found Department"] },
+  ];
