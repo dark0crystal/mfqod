b2d7206a970f3eea98a5eff343dafcabdff2194f
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-17 14:29:41 +0400

    adding form

--- src/app/[locale]/(protected)/dashboard/manage-users/[userId]/AddUserManagement.tsx
+++ src/app/[locale]/(protected)/dashboard/manage-users/[userId]/AddUserManagement.tsx
@@ -1,7 +1,25 @@
+"use client"
+import { useForm } from "react-hook-form"
+
+
+type FormFields ={
+    place:string
+    org:string
+}
+
 export default function AddUserManagement(){
+
+    const {register} = useForm<FormFields>()
     return(
         <div>
             add user management
+            <div>
+                <form>
+                    <input type="text" {...register("place")}/>
+                    <input type="text" {...register("org")}/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
