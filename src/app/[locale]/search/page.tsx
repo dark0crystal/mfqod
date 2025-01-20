cf0ec2e93bcf7f5e152e361009a28640a459570f
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-20 22:56:27 +0400

    nice design

--- src/app/[locale]/search/page.tsx
+++ src/app/[locale]/search/page.tsx
@@ -71,7 +71,7 @@ export default function Search() {
   };
 
   return (
-    <div className="grid grid-cols-12 h-[85vh] bg-gray-100">
+    <div className="grid grid-cols-12 h-[85vh] ">
       {/* Left Section */}
       <div className="col-span-2  p-4 flex flex-col justify-center items-center">
          <div className="relative h-full w-full rounded-2xl overflow-hidden">
@@ -83,19 +83,26 @@ export default function Search() {
       </div>
 
       {/* Center Section */}
-      <div className="col-span-8 bg-white flex flex-col items-center justify-start p-8 overflow-y-auto">
-        <div>
-        <div>
-          <Image src={} alt="search bg image" fill objectFit="cover"/>
+      <div className="col-span-8  flex flex-col items-center  p-4 overflow-y-auto w-full h-full">
+        <div className="relative z-20 w-full flex items-center min-h-[35vh]">
+        <div className="absolute -z-10 w-full  rounded-xl overflow-hidden h-full">
+          <Image
+           src={bg1}
+            alt="search bg image"
+             fill 
+             objectFit="cover"
+             className=""
+             />
         </div>
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full max-w-md">
+        <div className="absolute bottom-12 left-12">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-row gap-2 w-full ">
           <div>
             <input
               id="item"
               {...register("item")}
               type="text"
               placeholder="هيش مغيّب ؟"
-              className="w-full p-3 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              className="w-full p-3 border bg-white/70 text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
             {errors.item && <p className="mt-2 text-xs text-red-500">{errors.item.message}</p>}
           </div>
@@ -104,11 +111,11 @@ export default function Search() {
               id="place"
               {...register("place")}
               defaultValue=""
-              className="w-full p-3 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              className="w-full p-3 border bg-white/70 text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
             >
               <option value="" disabled>هين تدرس؟</option>
-              <option value="SQU">SQU</option>
-              <option value="UTAS Muscat">UTAS Muscat</option>
+              <option value="SQU">جامعة السلطان قابوس</option>
+              <option value="UTAS Muscat">جامعة التقنية-مسقط</option>
               <option value="UTAS Nizwa">UTAS Nizwa</option>
               <option value="UTAS Ibra">UTAS Ibra</option>
               <option value="NIZWA Uni">NIZWA Uni</option>
@@ -118,11 +125,12 @@ export default function Search() {
           <button
             type="submit"
             disabled={isSubmitting}
-            className="w-full p-3 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 disabled:bg-gray-400"
+            className="w-fit p-3 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 disabled:bg-gray-400"
           >
             {isSubmitting ? "Loading..." : "Submit"}
           </button>
         </form>
+        </div>
         </div>
 
         <div className="w-full mt-6">
@@ -132,14 +140,14 @@ export default function Search() {
       </div>
 
       {/* Right Section */}
-      <div className="col-span-2 bg-slate-400 flex flex-col items-center overflow-y-auto p-4">
+      <div className="col-span-2  flex flex-col items-center overflow-y-auto p-4">
         <div className="flex flex-col gap-4">
           {orgName.map((name: string, index: any) => (
             <button
               key={index}
               onClick={() => handleClick(name)}
               className={`p-3 rounded-full transition-transform duration-300 ${
-                currentName === name ? "bg-violet-500 text-white scale-110" : "bg-violet-300"
+                currentName === name ? "bg-indigo-300 text-white scale-110" : "bg-violet-300"
               }`}
             >
               {name}
