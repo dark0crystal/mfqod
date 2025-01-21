fd6f35bba2e8d7b79db4b96478f96fc10b1092f6
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-21 17:03:44 +0400

    design the nav bar

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -8,6 +8,7 @@ import Brand from "./Brand";
 import  {SignIn}  from "../auth/sign-in"
 import { SignOut } from "../auth/sign-out";
 import Profile from "./Profile";
+import { FaSearch } from "react-icons/fa";
 
 
 
@@ -16,29 +17,32 @@ export default async function NavBar() {
   // const locale = (await getLocale()).substring(0, 2); // This will give you "ar" or "en"
 
   // const t = await getTranslations("Links");
-  const navLinks = [
-    { direction: "/search", name: "Search" },
-    { direction: "/", name: "Main" },
-    { direction: "/report-found-item", name:"report-found-item" },
-    // { direction: "/dashboard", name:"dashboard" },
-    
-  ];
+
 
   return (
-    <nav  className="flex items-center justify-center h-[15vh] max-h-[15vh]">
+    <nav  className="flex items-center justify-center h-[12vh] max-h-[12vh]">
       
-      <div className='grid grid-cols-12  p-2 lg:p-2 rounded-full w-full   '>
+      <div className='grid grid-cols-12  p-2 lg:p-2 rounded-full w-full'>
       
 
 
         {/* right section ar links */}
         <div className="flex items-center justify-center p-3 rounded-lg col-span-5 ">
-          {navLinks.map((navLink, index) => (
-            <Link href={navLink.direction} key={index}>
-              <h1 className="text-lg  mx-4 font-normal">{navLink.name}</h1>
+         
+            <Link href="/search" >
+              <div className="bg-slate-300/60 rounded-3xl p-2  mx-4">
+                  <h1 className="text-lg  font-normal">Search</h1>
+                  
+              </div>
             </Link>
-            
-          ))} 
+
+            <Link href="/report-found-item" >
+              <div className="bg-slate-300/60 rounded-3xl p-2  mx-4">
+                  <h1 className="text-lg  font-normal">Report</h1>
+                
+              </div>
+            </Link>
+      
         </div>
 
 
