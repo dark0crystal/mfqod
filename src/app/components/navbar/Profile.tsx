65930bc2f088f5ed858f29bf9cb55eaf4a02032d
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-22 12:03:20 +0400

    designn the profile

--- src/app/components/navbar/Profile.tsx
+++ src/app/components/navbar/Profile.tsx
@@ -1,15 +1,39 @@
+"use client"
 import { Link } from "@/i18n/routing";
 import {auth} from "../../../../auth"
-import  {SignIn}  from "../auth/sign-in"
-import { SignOut } from "../auth/sign-out";
-export default async function Profile(){
-    const session = auth();
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { SignOut } from "../auth/client/signout-button";
+import { SignIn } from "../auth/client/signin-button";
+import defaultProfileImage from "../../../../public/img2.jpeg"
+import { useState } from "react";
+export default  function Profile(){
+    const [show , setShow] = useState(false)
+    const session =useSession();
+    function handleProfile(){
 
+    }
 
     return(
         <div>
-             <SignIn/>
-             <SignOut/>
+            {session.status == "unauthenticated" ? (
+                <div>
+                    <SignIn/>
+                </div>
+             
+            ):(
+                <div>
+                    <button onClick={handleProfile} className="relative w-[40px] h-[40px] overflow-hidden rounded-xl"> 
+                        pro
+                        <Image fill objectFit="cover" alt="profile image" src={session.data?.user?.image || defaultProfileImage }/>
+                    </button>
+                     <SignOut/>
+                </div>
+                 
+                
+            )}
+          
+
         </div>
     )
 
