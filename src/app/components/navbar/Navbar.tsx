bd33e11298e8254fbe4e38046d58ef69d8e9c097
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-23 11:14:21 +0400

    links responsiveness

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -23,14 +23,14 @@ export default async function NavBar() {
          
             <Link href="/search" >
               <div className="p-2  mx-4">
-                  <h1 className="text-md text-gray-700 font-normal hover:text-blue-600">{t("search")}</h1>
+                  <h1 className="text-[0.9rem] lg:text-[1rem] text-md text-gray-700 font-normal hover:text-blue-600">{t("search")}</h1>
                   
               </div>
             </Link>
 
             <Link href="/report-found-item" >
               <div className="p-2  mx-4">
-                  <h1 className="text-md text-gray-700 font-normal hover:text-blue-600">{t("report")}</h1>
+                  <h1 className="text-[1rem] text-gray-700 font-normal hover:text-blue-600">{t("report")}</h1>
               </div>
             </Link>
       
