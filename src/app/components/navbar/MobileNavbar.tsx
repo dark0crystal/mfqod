2bfdfdd3ecc54a93ddda118a43efa5597528e788
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-23 14:18:36 +0400

    mobile nav bar

--- src/app/components/navbar/MobileNavbar.tsx
+++ src/app/components/navbar/MobileNavbar.tsx
@@ -42,3 +42,32 @@
 //     </div>
 //   );
 // }
+"use client"
+import { Link } from "@/i18n/routing"
+import { useTranslations } from "next-intl"
+export default function MobileNavbar(){
+    const t = useTranslations("navbar")
+    return(
+        <nav  className="flex items-center justify-center h-[12vh] max-h-[12vh] md:hidden ">
+            <div>
+                 {/* right section ar links */}
+                        <div className="flex items-center justify-center p-3 rounded-lg col-span-5 ">
+                         
+                            <Link href="/search" >
+                              <div className="p-2  mx-4">
+                                  <h1 className="text-[0.9rem] lg:text-[1rem] text-md text-gray-700 font-normal hover:text-blue-600">{t("search")}</h1>
+                                  
+                              </div>
+                            </Link>
+                
+                            <Link href="/report-found-item" >
+                              <div className="p-2  mx-4">
+                                  <h1 className="text-[1rem] text-gray-700 font-normal hover:text-blue-600">{t("report")}</h1>
+                              </div>
+                            </Link>
+                      
+                        </div>
+            </div>
+        </nav>
+    )
+}
