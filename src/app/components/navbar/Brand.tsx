48542cfb919143948cd953dc95d963d3f9e379d5
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-23 11:13:59 +0400

    coloring  the brand formation

--- src/app/components/navbar/Brand.tsx
+++ src/app/components/navbar/Brand.tsx
@@ -17,8 +17,8 @@ export default  function Brand(){
     // const locale = (await getLocale()).substring(0,2)
     // const t = await getTranslations("HomePage")
     return(
-        <div className="mx-6 text-black text-[20px] md:text-[30px] lg:text-[40px] ">
-            <Link   className={lalezarFont.className} href='/'><p>{t("brand")} </p><span>{t("brand-duplicate")}</span></Link>
+        <div className="mx-6 text-black text-[20px] md:text-[30px] lg:text-[42px] ">
+            <Link   className={lalezarFont.className} href='/'><p className="flex flex-col relative z-20 text-slate-800">{t("brand-duplicate")} <span className="absolute -z-10 text-blue-600">{t("brand")}</span></p></Link>
         </div>
     )
 }
