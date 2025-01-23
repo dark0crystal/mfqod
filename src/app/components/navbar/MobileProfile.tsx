fc714b20c26a7667c748b24ed590672365db5dfa
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-23 22:32:00 +0400

    fix

---
+++ src/app/components/navbar/MobileProfile.tsx
@@ -0,0 +1,25 @@
+// import { Link } from "@/i18n/routing"
+// export default function MobileProfile(){
+    
+//     return(
+//         <div>
+//              <div
+//                           className="absolute bottom-[60px] left-1/2 transform -translate-x-1/2 w-48 bg-white shadow-lg rounded-lg p-4 z-50"
+//                           onClick={() => setShow(false)}
+//                         >
+//                           <div className="flex flex-col items-center text-gray-800 space-y-2">
+//                             {/* User Dashboard */}
+//                             <Link href="/dashboard" className="text-sm font-medium hover:text-violet-500">
+//                               Dashboard
+//                             </Link>
+            
+//                             {/* User Email */}
+//                             <p className="text-sm text-gray-600">{session?.user?.email || "No email provided"}</p>
+            
+//                             {/* Sign Out Button */}
+//                             <SignOut />
+//                           </div>
+//                         </div>
+//         </div>
+//     )
+// }
