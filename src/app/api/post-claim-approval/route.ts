66574357bd44d1b2876378ce25485a620a46e15f
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-25 11:50:53 +0400

    new calim approval api

--- src/app/api/post-claim-approval/route.ts
+++ src/app/api/post-claim-approval/route.ts
@@ -1,4 +1,27 @@
+import prisma from "@/lib/db";
 import { NextResponse ,NextRequest } from "next/server";
-export default function PUT(){
-    
-}
+export async function PATCH(req: Request) {
+    try {
+      const { postId, approval } = await req.json();
+  
+      if (!postId || approval === undefined) {
+        return NextResponse.json(
+          { error: "Invalid request payload" },
+          { status: 400 }
+        );
+      }
+  
+      const updatedPost = await prisma.claim.update({
+        where: { id: postId },
+        data: { approval },
+      });
+  
+      return NextResponse.json(updatedPost);
+    } catch (error) {
+      console.error("Error updating post approval:", error);
+      return NextResponse.json(
+        { error: "Failed to update post approval" },
+        { status: 500 }
+      );
+    }
+  }
