6b4b9a6a434e981d7fd1fd542e679dfa2216ab88
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-25 13:18:52 +0400

    fixing the api route

--- src/app/api/post-claim-approval/route.ts
+++ src/app/api/post-claim-approval/route.ts
@@ -2,9 +2,9 @@ import prisma from "@/lib/db";
 import { NextResponse ,NextRequest } from "next/server";
 export async function PATCH(req: Request) {
     try {
-      const { postId, approval } = await req.json();
+      const { claimId, approval } = await req.json();
   
-      if (!postId || approval === undefined) {
+      if (!claimId || approval === undefined) {
         return NextResponse.json(
           { error: "Invalid request payload" },
           { status: 400 }
@@ -12,15 +12,15 @@ export async function PATCH(req: Request) {
       }
   
       const updatedPost = await prisma.claim.update({
-        where: { id: postId },
-        data: { approval },
+        where: { id: claimId },
+        data: { approved:approval },
       });
   
       return NextResponse.json(updatedPost);
     } catch (error) {
-      console.error("Error updating post approval:", error);
+      console.error("Error updating claim approval:", error);
       return NextResponse.json(
-        { error: "Failed to update post approval" },
+        { error: "Failed to update claim approval" },
         { status: 500 }
       );
     }
