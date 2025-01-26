0b1eafaec006f2bbe86f99ceb91d8d3b8162c125
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-26 18:49:39 +0400

    fixing layout

--- src/app/[locale]/(protected)/dashboard/layout.tsx
+++ src/app/[locale]/(protected)/dashboard/layout.tsx
@@ -1,28 +1,21 @@
-
-import MobileSideBar from "./MobileSideBar";
 import NavBar from "./NavBar";
 
-export default async function Layout({children}: Readonly<{ children: React.ReactNode;}>)  {
-  
+export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <main>
+      <div className="grid grid-cols-1 lg:grid-cols-3 h-screen">
+        {/* Sidebar for large screens */}
+        <aside className="flex-row lg:flex lg:flex-col items-center px-4 border-r-2 lg:col-span-1">
+          <NavBar />
+        </aside>
 
-      <main>
-      <div className="grid grid-cols-3 h-screen">
-         <div className=" border bottom-2 lg:flex flex-col  items-center px-4 hidden lg:col-span-1">
-            
-            <NavBar />
-        </div>
         
-        {/* <MobileSideBar/> */}
 
-        <div className=" flex flex-col  items-center px-4 col-span-3  lg:col-span-2">
-            {children}
-        </div>
-        
-        </div>
-      </main>
-
-    </>
-  )
+        {/* Main content */}
+        <section className="col-span-1 lg:col-span-2 flex flex-col items-center px-4 overflow-y-auto">
+          {children}
+        </section>
+      </div>
+    </main>
+  );
 }
