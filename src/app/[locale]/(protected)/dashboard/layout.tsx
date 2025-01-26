42327b1d9a4c3d6703eef03ba3f69859a7f9f974
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-26 13:04:32 +0400

    fix

--- src/app/[locale]/(protected)/dashboard/layout.tsx
+++ src/app/[locale]/(protected)/dashboard/layout.tsx
@@ -1,19 +1,19 @@
-import NavBar from "./NavBar";
 
-import UserInfo from "./UserInfo";
+import NavBar from "./NavBar";
 export default async function Layout({children}: Readonly<{ children: React.ReactNode;}>)  {
+
   
-   
   return (
     <>
 
       <main>
       <div className="grid grid-cols-3 h-screen">
-         <div className=" border bottom-2 flex flex-col  items-center px-4 col-span-1">
+         <div className=" border bottom-2 lg:flex flex-col  items-center px-4 hidden lg:col-span-1">
             
             <NavBar />
         </div>
-        <div className=" flex flex-col  items-center px-4  col-span-2">
+       
+        <div className=" flex flex-col  items-center px-4 col-span-3  lg:col-span-2">
             {children}
         </div>
         
