295b978f901462f6d7a0e412aa3c9d7dfed5875c
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-26 13:04:42 +0400

    mobile side bar

---
+++ src/app/[locale]/(protected)/dashboard/MobileSideBar.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import NavBar from "./NavBar"
+export default function MobileSideBar(){
+
+    const [show ,setShow] = useState(false)
+
+   function handleShow(){
+    setShow(!show)
+   }
+
+    return(
+        <div>
+        <button onClick={handleShow}>
+        nav
+      </button>
+      {show &&
+        <div className="h-screen w-screen bg-white">
+            <NavBar />
+        </div>
+      }
+
+        </div>
+    )
+}
