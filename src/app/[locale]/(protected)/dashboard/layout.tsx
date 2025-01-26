35ff92505d67a81ac732b8a8c93db7c294a3c2f4
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-26 22:05:39 +0400

    fixing layout navbar height

--- src/app/[locale]/(protected)/dashboard/layout.tsx
+++ src/app/[locale]/(protected)/dashboard/layout.tsx
@@ -2,10 +2,10 @@ import NavBar from "./NavBar";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
-      <div className="grid grid-cols-1 lg:grid-cols-3 h-screen">
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 h-[88vh]">
         {/* Sidebar for large screens */}
-        <aside className="flex-row lg:flex lg:flex-col items-center px-4 border-r-2 lg:col-span-1">
+        <aside className="flex-row lg:flex lg:flex-col items-center px-4 border-r-2 lg:col-span-1 ">
           <NavBar />
         </aside>
 
@@ -16,6 +16,6 @@ export default async function Layout({ children }: { children: React.ReactNode }
           {children}
         </section>
       </div>
-    </main>
+    </div>
   );
 }
