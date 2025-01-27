e860b62e89d3ad1d8dddc20c56767973db83b1fc
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-27 10:56:07 +0400

    footer structure

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,19 +1,62 @@
+import Brand from "./navbar/Brand"
+import { Link } from "@/i18n/routing";
 // components/Footer.js
 export default function Footer() {
     return (
       <footer className=" text-gray-500 py-10 mt-20 border-t-2">
+        <div className="flex flex-col lg:flex-row">
+       
 
-        {/* brand */}
-        <div>
-          <h1></h1>
-          <p></p>
+            {/* brand and lang change */}
+            <div>
+              <Brand/>
+              <p className="text-sm text-gray-500">مفقود هو موقع لمساعدة الأشخاص للحصول على مفقوداتهم</p>
+            </div>
+
+            {/* Links */}
+            <div>
+              {/*  */}
+              <div>
+                <h1>روابط سريعة</h1>
+                  <div>
+                    <Link href="/">ابحث</Link>
+                  </div>
+                  <div>
+                    <Link href="/">بلغ</Link>
+                  </div>
+                  <div>
+                    <Link href="/">اي شي</Link>
+                  </div>
+              </div>
+              {/*  */}
+              <div>
+                <h1>الخصوصية و البنود</h1>
+                  <div>
+                    <Link href="/"></Link>
+                  </div>
+                  <div>
+                    <Link href="/"></Link>
+                  </div>
+                  <div>
+                    <Link href="/"></Link>
+                  </div>
+              </div>
+              {/*  */}
+              <div>
+                <h1>أشياء أخرى</h1>
+                  <div>
+                    <Link href="/">رابط</Link>
+                  </div>
+                  <div>
+                    <Link href="/">رابط</Link>
+                  </div>
+                  <div>
+                    <Link href="/">رابط</Link>
+                  </div>
+              </div>
+            </div>
+         
         </div>
-
-        {/* Links */}
-        <div>
-
-        </div>
-
 
       </footer>
     );
