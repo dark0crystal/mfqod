5c3b9dfe47e9680c37fbeb0276b7cf06526513e2
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-27 17:21:02 +0400

    remove

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -48,7 +48,7 @@ export default async function Home() {
           
 
           </p>
-        <div className="flex flex-col items-center justify-center mt-2 rounded-lg  ">
+        <div className="flex flex-col items-center justify-center mt-2 rounded-lg  px-[2rem] md:px-[5rem] lg:px-[7rem] text-center">
           <p className="text-center text-lg font-semibold text-black">
           مغيّب شي ؟ مفقود بيساعدك 
           مغيّب شي ؟ مفقود بيساعدك 
@@ -56,7 +56,7 @@ export default async function Home() {
           <p className="text-center text-lg font-semibold text-black">
           مغيّب شي ؟ مفقود بيساعدك 
           </p>
-          <h1>{t("disclaimer")}</h1>
+          
         </div>
 
         {/* Bottom Center Card */}
