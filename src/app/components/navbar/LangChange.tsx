413e0b1ea88f769e1f3407a9e5691d329fce5983
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-28 17:25:12 +0400

    adding a language change component

--- src/app/components/navbar/LangChange.tsx
+++ src/app/components/navbar/LangChange.tsx
@@ -1,30 +1,30 @@
-// 'use client'
-// import { usePathname } from "next/navigation";
-// import Link from "next/link";
-// import {  useLocale } from "next-intl";
-// // import { Inter } from "next/font/google";
+'use client'
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { useTranslations, useLocale } from "next-intl";
+// import { Inter } from "next/font/google";
 
-// // const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] ,display: 'swap',});
+// const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] ,display: 'swap',});
 
-// export default function LanguageChange() {
-//     // const t = useTranslations("Links");
-//     const pathName = usePathname();
-//     const locale = useLocale();
+export default function LanguageChange() {
+    const t = useTranslations("Links");
+    const pathName = usePathname();
+    const locale = useLocale();
 
-//     // Remove the current locale from the pathname
-//     const pathWithoutLocale = pathName.replace(`/${locale}`, "");
+    // Remove the current locale from the pathname
+    const pathWithoutLocale = pathName.replace(`/${locale}`, "");
 
-//     return (
-//         <div >
-//             {locale === "ar" ? (
-//                 <Link className="mx-6 text-3xl font-bold" href={`/en${pathWithoutLocale}`} locale="en">
-//                     🇬🇧
-//                 </Link>
-//             ) : (
-//                 <Link className="mx-6 text-3xl font-bold" href={`/ar${pathWithoutLocale}`} locale="ar">
-//                     🇴🇲
-//                 </Link>
-//             )}
-//         </div>
-//     );
-// }
+    return (
+        <div >
+            {locale === "ar" ? (
+                <Link className="mx-6 text-3xl font-bold" href={`/en${pathWithoutLocale}`} locale="en">
+                    🇬🇧
+                </Link>
+            ) : (
+                <Link className="mx-6 text-3xl font-bold" href={`/ar${pathWithoutLocale}`} locale="ar">
+                    🇴🇲
+                </Link>
+            )}
+        </div>
+    );
+}
