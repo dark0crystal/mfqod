6b7442443ca22fd5ab1511a7170f9fdb261dd66e
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-19 12:04:48 +0400

    add loading

--- src/app/[locale]/search/page.tsx
+++ src/app/[locale]/search/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+"use client"
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
@@ -9,12 +8,13 @@ import DisplayPosts from "./DisplayPosts";
 import DataProvider from "@/app/storage";
 import Footer from "@/app/components/Footer";
 import Image from "next/image";
-import ad from "../../../../public/ad.png"
-import bg11 from "../../../../public/bg11.jpg"
-import bg12 from "../../../../public/bg12.jpeg"
+import ad from "../../../../public/ad.png";
+import bg11 from "../../../../public/bg11.jpg";
+import bg12 from "../../../../public/bg12.jpeg";
 import { FaSearch } from "react-icons/fa";
 import { RiLoader2Line } from "react-icons/ri";
 import { useTranslations } from "next-intl";
+
 const schema = z.object({
   item: z.string().min(1, { message: "The Field is required!" }),
   place: z.string().min(1, { message: "Please select a place" }),
@@ -23,41 +23,42 @@ const schema = z.object({
 type FormFields = z.infer<typeof schema>;
 
 export default function Search() {
-  
   const [items, setItems] = useState<any[]>([]);
-  const router = useRouter();
   const [currentName, setCurrentName] = useState<string>();
-  const [show , setShow] = useState(false);
+  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false); // Loading state
 
   const { orgNames } = DataProvider();
-
-  const t=  useTranslations("search")
+  const t = useTranslations("search");
 
   const handleClick = async (name: string) => {
     setCurrentName(name);
     await fetchItemByPlace(name);
   };
 
-  function handleShow(){
-    setShow(!show)
-  }
+  const handleShow = () => {
+    setShow(!show);
+  };
 
   const fetchItems = async (item: string, place: string) => {
+    setLoading(true); // Start loading
     try {
       const response = await fetch(`/api/get-items?item=${item}&orgName=${place}`);
       if (response.ok) {
         const data = await response.json();
-        console.log(data)
         setItems(data);
       } else {
         console.error("Failed to fetch items");
       }
     } catch (error) {
       console.error("Error fetching items:", error);
+    } finally {
+      setLoading(false); // Stop loading
     }
   };
 
   const fetchItemByPlace = async (place: string) => {
+    setLoading(true); // Start loading
     try {
       const response = await fetch(`/api/get-items-by-place?orgName=${place}`);
       if (response.ok) {
@@ -68,6 +69,8 @@ export default function Search() {
       }
     } catch (error) {
       console.error("Error fetching items:", error);
+    } finally {
+      setLoading(false); // Stop loading
     }
   };
 
@@ -76,23 +79,26 @@ export default function Search() {
   });
 
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    setLoading(true); // Start loading
     console.log(data);
     await fetchItems(data.item, data.place);
     reset();
+    setLoading(false); // Stop loading
   };
 
-  return ( 
-    <div className="relative  lg:grid lg:grid-cols-12 lg:h-[88vh] ">
-
-        {/* left Section */}
-        <div className="hidden lg:col-span-2  lg:flex flex-col items-center overflow-y-auto p-4">
+  return (
+    <div className="relative lg:grid lg:grid-cols-12 lg:h-[88vh]">
+      {/* Left Section */}
+      <div className="hidden lg:col-span-2 lg:flex flex-col items-center overflow-y-auto p-4">
         <div className="flex flex-col gap-4">
           {orgNames.map((org, index: any) => (
             <button
               key={index}
               onClick={() => handleClick(org.key)}
               className={`p-3 rounded-full transition-transform duration-300 text-sm ${
-                currentName === org.key ? "bg-blue-100 border border-blue-400  p-3 rounded-md scale-110" : "bg-white border border-blue-200  p-3 rounded-md "
+                currentName === org.key
+                  ? "bg-blue-100 border border-blue-400 p-3 rounded-md scale-110"
+                  : "bg-white border border-blue-200 p-3 rounded-md"
               }`}
             >
               {org.name}
@@ -101,95 +107,97 @@ export default function Search() {
         </div>
       </div>
 
-      {/* for mobile and ipad view */}
+      {/* For mobile and ipad view */}
       <div className="z-40 fixed bottom-4 right-4 lg:hidden">
-        
-        {show && 
-      <div className="relative flex flex-col gap-2">
-          {orgNames.map((org, index: any) => (
-            <button
-              key={index}
-              onClick={() => handleClick(org.key)}
-              className={`px-3 py-2 rounded-full transition-transform duration-300 text-sm ${
-                currentName === org.key ? "bg-blue-200 border border-blue-400 rounded-md scale-105" : "bg-white border border-blue-200   rounded-md "
-              }`}
-            >
-              {org.name}
-            </button>
-          ))}
-        </div>
-        }
-       <button 
-        className="bg-white border-2 border-blue-500  py-3 px-4 rounded-md mt-2 text-lg font-semibold" 
-        onClick={handleShow}
-       >
-          {t("filter")}
-      </button>
-
-      </div>
-        
-      {/* Center Section */}
-<div className="col-span-12 lg:col-span-8 flex flex-col items-center p-4 overflow-y-auto w-full h-full">
-  <div className="relative z-20 w-full flex items-center min-h-[30vh] justify-center">
-    <div className="absolute -z-10 w-full rounded-xl overflow-hidden h-full">
-      
-      <Image
-        src={bg12}
-        alt="search bg image"
-        fill
-        objectFit="cover"
-      /> 
-    </div>
-    {/* blur background on top of image */}
-    <div className="absolute z-10 bg-white/40 w-full h-full rounded-xl"/>
-
-    <div className="lg:absolute lg:bottom-12  w-full flex justify-center z-20">
-      <form onSubmit={handleSubmit(onSubmit)} className="flex lg:flex-row flex-col gap-2 w-fit max-w-md">
-        <div>
-          <input
-            id="item"
-            {...register("item")}
-            type="text"
-            placeholder="هيش مغيّب ؟"
-            className="w-full p-3 border bg-slate-50 text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          {errors.item && <p className="mt-2 text-xs text-red-500">{errors.item.message}</p>}
-        </div>
-        <div>
-          <select
-            id="place"
-            {...register("place")}
-            defaultValue=""
-            className="w-full p-3 border bg-slate-50 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            <option value="" disabled>هين تدرس؟</option>
+        {show && (
+          <div className="relative flex flex-col gap-2">
             {orgNames.map((org, index: any) => (
-              <option key={index} value={org.key}>{org.name}</option>
+              <button
+                key={index}
+                onClick={() => handleClick(org.key)}
+                className={`px-3 py-2 rounded-full transition-transform duration-300 text-sm ${
+                  currentName === org.key
+                    ? "bg-blue-200 border border-blue-400 rounded-md scale-105"
+                    : "bg-white border border-blue-200 rounded-md"
+                }`}
+              >
+                {org.name}
+              </button>
             ))}
-          </select>
-          {errors.place && <p className="mt-2 text-xs text-red-500">{errors.place.message}</p>}
-        </div>
+          </div>
+        )}
         <button
-          type="submit"
-          disabled={isSubmitting}
-          className="w-full lg:w-fit lg:h-fit p-3 bg-blue-600/90 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 disabled:bg-gray-400"
+          className="bg-white border-2 border-blue-500 py-3 px-4 rounded-md mt-2 text-lg font-semibold"
+          onClick={handleShow}
         >
-          {isSubmitting ? <RiLoader2Line size={26} /> : <FaSearch size={26} />}
+          {t("filter")}
         </button>
-      </form>
-    </div>
-  </div>
+      </div>
 
-  <div className="w-full mt-6">
-    <DisplayPosts items={items} />
-    <Footer />
-  </div>
-</div>
+      {/* Center Section */}
+      <div className="col-span-12 lg:col-span-8 flex flex-col items-center p-4 overflow-y-auto w-full h-full">
+        <div className="relative z-20 w-full flex items-center min-h-[30vh] justify-center">
+          <div className="absolute -z-10 w-full rounded-xl overflow-hidden h-full">
+            <Image
+              src={bg12}
+              alt="search bg image"
+              fill
+              objectFit="cover"
+            />
+          </div>
+          {/* Blur background on top of image */}
+          <div className="absolute z-10 bg-white/40 w-full h-full rounded-xl" />
 
+          <div className="lg:absolute lg:bottom-12 w-full flex justify-center z-20">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex lg:flex-row flex-col gap-2 w-fit max-w-md">
+              <div>
+                <input
+                  id="item"
+                  {...register("item")}
+                  type="text"
+                  placeholder="هيش مغيّب ؟"
+                  className="w-full p-3 border bg-slate-50 text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                {errors.item && <p className="mt-2 text-xs text-red-500">{errors.item.message}</p>}
+              </div>
+              <div>
+                <select
+                  id="place"
+                  {...register("place")}
+                  defaultValue=""
+                  className="w-full p-3 border bg-slate-50 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+                  <option value="" disabled>هين تدرس؟</option>
+                  {orgNames.map((org, index: any) => (
+                    <option key={index} value={org.key}>{org.name}</option>
+                  ))}
+                </select>
+                {errors.place && <p className="mt-2 text-xs text-red-500">{errors.place.message}</p>}
+              </div>
+              <button
+                type="submit"
+                disabled={isSubmitting || loading} // Disable button during loading or submitting
+                className="w-full lg:w-fit lg:h-fit p-3 bg-blue-600/90 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 disabled:bg-gray-400"
+              >
+                {loading ? <RiLoader2Line size={26} className="animate-spin" /> : <FaSearch size={26} />}
+              </button>
+            </form>
+          </div>
+        </div>
+
+        <div className="w-full mt-6">
+          {loading ? (
+            <div className="text-center text-gray-500">Loading...</div>
+          ) : (
+            <DisplayPosts items={items} />
+          )}
+          <Footer />
+        </div>
+      </div>
 
       {/* Right Section */}
-      <div className="hidden lg:col-span-2  p-4 lg:flex flex-col justify-center items-center">
-         {/* <div className="relative h-full w-full rounded-2xl overflow-hidden">
+      <div className="hidden lg:col-span-2 p-4 lg:flex flex-col justify-center items-center">
+        {/* <div className="relative h-full w-full rounded-2xl overflow-hidden">
           <Image alt="ad" src={ad} fill objectFit="cover"/>
          <div className="absolute bottom-0 bg-gray-300/20 text-center w-full rounded-t-2xl h-10"> 
             <h1 className="text-lg text-white  font-normal">Fake Sponsorship</h1>
