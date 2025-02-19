cb4d88b5c76c36d61cc8e68c863d52fa938885ac
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-19 12:05:02 +0400

    design the splash screen

--- src/app/components/Splash.tsx
+++ src/app/components/Splash.tsx
@@ -1,7 +1,7 @@
-
 "use client";
 
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Splash() {
   const [isVisible, setIsVisible] = useState(true);
@@ -10,7 +10,7 @@ export default function Splash() {
     // Hide the splash screen after 3 seconds
     const timer = setTimeout(() => {
       setIsVisible(false);
-    }, 1000);
+    }, 3000); // Splash screen lasts for 3 seconds
 
     return () => clearTimeout(timer);
   }, []);
@@ -19,13 +19,47 @@ export default function Splash() {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-indigo-600 text-white">
-      {/* Logo or App Name */}
-      <div className="flex flex-col items-center animate-fade">
-        <div className="text-4xl font-bold mb-4">مفقود</div>
-        {/* Loading Spinner */}
-        <div className="w-10 h-10 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
-      </div>
-    </div>
+    <motion.div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-indigo-400 text-white"
+      initial={{ opacity: 1 }}
+      animate={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 3 }}
+    >
+      <motion.div
+        className="flex items-center space-x-4"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
+       
+        <motion.div
+          className="text-4xl font-bold"
+          initial={{ x: 100 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1, type: "spring", stiffness: 100 }}
+        >
+          مفقود
+        </motion.div>
+        <motion.div
+          className="text-4xl font-bold"
+          initial={{ x: 100 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1, type: "spring", stiffness: 100 }}
+        >
+          <span className="mx-4">|</span>
+        </motion.div>
+
+        <motion.div
+          className="text-4xl font-bold"
+          initial={{ x: -100 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 1, type: "spring", stiffness: 100 }}
+        >
+          mfqod
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 }
