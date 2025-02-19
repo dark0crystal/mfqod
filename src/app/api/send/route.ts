1899716417486d59a38f534236ba85a76547e175
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-19 09:39:45 +0400

    fix

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'oman@mfqod.com',
+      from: 'Acme <oman@mfqod.com>',
       to: ['albusaidi9094@gmail.com'],
       subject: 'Hello world',
       react: EmailTemplate({ firstName: 'John' }),
